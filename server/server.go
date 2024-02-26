ab70b9075a2d4290a12ca72cc5395966f3051568
Surya Patil <patil.surya01@gmail.com>
2024-02-26 16:00:07 -0500

    changed server code

--- server/server.go
+++ server/server.go
@@ -1,161 +1,124 @@
-package server
+package main
 
-// formatting and printing values to the console.
 import (
 	"context"
-	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
-	"sync"
-	"time"
+) 
 
-	pb "peer-node/fileshare"
+const keyServerAddr = "serverAddr"
 
-	"google.golang.org/grpc"
-)
 
-// Used for build HTTP servers and clients.
-type fileShareServerNode struct {
-	pb.UnimplementedFileShareServer
-	savedFiles   map[string][]*pb.FileDesc // read-only after initialized
-	mu           sync.Mutex                // protects routeNotes
-	currentCoins float64
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /root request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
 
-func NotifyAvailableStorage(client pb.FileShareClient, storageIP *pb.StorageIP) {
-	log.Printf("Sending Market Availability")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	stream, err := client.NotifyAvailableStorage(ctx, storageIP)
-	if err != nil {
-		log.Fatalf("client.ListFeatures failed: %v", err)
-	}
-	for {
-		fileDesc, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
+func getFile(w http.ResponseWriter, r *http.Request) {
+	// Get the context from the request
+	ctx := r.Context()
+
+	// Check if the "filename" query parameter is present
+	hasFilename := r.URL.Query().Has("filename")
+
+	// Retrieve the value of the "filename" query parameter
+	filename := r.URL.Query().Get("filename")
+
+	// Print information about the request
+	fmt.Printf("%s: got /file request. filename(%t)=%s\n",
+		ctx.Value(keyServerAddr),
+		hasFilename, filename,
+	)
+
+	// Check if the "filename" parameter is present
+	if hasFilename {
+
+		// Open the file
+		filePath := filepath.Join(".", filename)
+		file, err := os.Open(filePath)
 		if err != nil {
-			log.Fatalf("client.notifyAvailableStorage failed: %v", err)
-		}
-		log.Printf("Recieved File %s", fileDesc.FileName)
-	}
-}
-
-func sendFile(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		for k, v := range r.URL.Query() {
-			fmt.Printf("%s: %s\n", k, v)
-		}
-		// file = r.URL.Query().Get("filename")
-		w.Write([]byte("Received a GET request\n"))
-	case "POST":
-		reqBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s\n", reqBody)
-		w.Write([]byte("Received a POST request\n"))
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
-	}
-	w.Write([]byte("Received a GET request\n"))
-	filename := r.URL.Path[len("/reqFile/"):]
-
-	// Open the file
-	file, err := os.Open(filename)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	defer file.Close()
-
-	// Set content type
-	contentType := "application/octet-stream"
-	switch {
-	case filename[len(filename)-4:] == ".txt":
-		contentType = "text/plain"
-	case filename[len(filename)-5:] == ".json":
-		contentType = "application/json"
-		// Add more cases for other file types if needed
-	}
-
-	// Set content disposition header
-	w.Header().Set("Content-Disposition", "attachment; filename="+filename)
-	w.Header().Set("Content-Type", contentType)
-
-	// Copy file contents to response body
-	_, err = io.Copy(w, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-type sentCoinMessage struct {
-	Amount string `json:"amount"`
-}
-
-func receiveCoin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "HANDLE COIN")
-	switch r.Method {
-	case "POST":
-		reqBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		var coins sentCoinMessage
-		err = json.Unmarshal(reqBody, &coins)
-		if err != nil {
-			http.Error(w, "Error decoding JSON data", http.StatusBadRequest)
+			// Handle error (file not found, permission issues, etc.)
+			http.Error(w, "File not found", http.StatusNotFound)
 			return
 		}
-		fmt.Printf("%s\n", reqBody)
-		w.Write([]byte("Received a POST request\n"))
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+		defer file.Close()
+
+		// Check if the "chunksize" query parameter is present
+		hasChunksize := r.URL.Query().Has("chunksize")
+
+		if hasChunksize {
+			// Retrieve the value of the "filename" query parameter
+			chunkSizeParam := r.URL.Query().Get("chunksize")
+			// Convert the value to an integer
+			chunkSize, err := strconv.Atoi(chunkSizeParam)
+			if err != nil {
+				// Handle the error (invalid value)
+				fmt.Printf("Invalid chunksize parameter value:", chunkSizeParam)
+				return
+			} 
+
+			fmt.Printf("chunkSize: %d\n", chunkSize)
+
+			buffer := make([]byte, chunkSize)
+
+			for {
+				// Read 10 bytes from the file
+				n, err := file.Read(buffer)
+				if err != nil {
+					// Check if it's the end of the file
+					if err.Error() == "EOF" {
+						break
+					}
+					// Handle other errors
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+	
+				// Write the 10-byte chunk to the response
+				w.Write(buffer[:n])
+				w.Write([]byte("\n***\n"))
+			}
+		} else{
+			fmt.Printf("No chunk size specified\n")
+			// Serve the file using http.ServeFile
+			http.ServeFile(w, r, filePath)
+			fmt.Printf("Served %s to client\n", filename)
+			return
+		}
+
+	} else {
+		// Write a response indicating that no filename was found
+		io.WriteString(w, "No filename found\n")
 	}
 }
-func setupProducer(gRPCPort int, httpPort int) *fileShareServerNode {
-	s := &fileShareServerNode{savedFiles: make(map[string][]*pb.FileDesc)}
-	// s.loadMappings(*jsonDBFile) // Have a load and save mappings
-	http.HandleFunc("/requestFile", sendFile)
-	http.HandleFunc("/sendCoin", receiveCoin)
-	fmt.Println("[Server]: Listening On Port" + strconv.Itoa(httpPort))
-	fmt.Println("[Server]: Press CTRL + C to quit.")
-	go func() {
-		for {
-			http.ListenAndServe(":"+strconv.Itoa(httpPort), nil)
-		}
-	}()
-	return s
-}
-
-// Can add back in TLS later
-var (
-	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
-	gRPCPort   = flag.Int("gport", 50051, "The gRPC port for send/receive gRPC")
-	httpPort   = flag.Int("hport", 50052, "The server port for listening for HTTP requests")
-)
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *gRPCPort))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/requestFile", getFile)
+	
+	ctx := context.Background()
+	server := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterFileShareServer(grpcServer, setupProducer(*gRPCPort, *httpPort))
-	grpcServer.Serve(lis)
+
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server: %s\n", err)
+	}
 }
+
+
