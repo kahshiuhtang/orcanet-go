a6739da2ae0a76ef1c2126a7aa51fce55fa104b5
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-03-17 11:11:16 -0400

    Add: GetNodeInfo for the peer node itself, not other nodes yet

--- internal/status/status.go
+++ internal/status/status.go
@@ -1,9 +1,12 @@
 package status
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/cbergoon/speedtest-go"
 )
@@ -15,15 +18,44 @@ type NetworkStatus struct {
 	LatencyMs         float64
 }
 
-type PeerNodeData struct {
-	isMe        bool
-	balance     float64
-	publicKey   string
-	location    string
-	isConnected bool
+type PeerNodeFileData struct {
+	IsMe          bool    `json:"is_me"`
+	Balance       float64 `json:"balance"`
+	PublicKey     string  `json:"public_key"`
+	PublicKeyPath string  `json:"public_key_path"`
+}
+type PeerNode struct {
+	IsMe      bool
+	Balance   float64
+	PublicKey string
 }
 
-func getNodeInfo() {
+func GetNodeInfo() PeerNode {
+	jsonFile, err := os.Open("config/self.json")
+	if err != nil {
+		fmt.Println("Error on loading config, please try again")
+	}
+	var meData PeerNodeFileData
+	byteValue, _ := io.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &meData)
+	defer jsonFile.Close()
+	var me PeerNode
+	me.Balance = meData.Balance
+	me.IsMe = meData.IsMe
+	me.PublicKey = meData.PublicKey
+	if me.PublicKey == "" {
+		content, error := os.ReadFile(meData.PublicKeyPath)
+		if error != nil {
+			fmt.Println("Error: unable to read in ")
+			return PeerNode{}
+		}
+		pub_key := string(content)
+		me.PublicKey = pub_key
+	}
+	return me
+}
+
+func GetPeerNodeInfo() {
 
 }
 
