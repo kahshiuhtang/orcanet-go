3030040bbb734d572217c7c4220b30ccbd5b67cf
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-03-17 10:45:37 -0400

    Update: test.go to create blockchain server

--- test/test.go
+++ test/test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"log"
 	orcaClient "orca-peer/internal/client"
+
 	pb "orca-peer/internal/fileshare"
 	"sync"
 
@@ -31,4 +32,21 @@ func main() {
 	defer conn.Close()
 	client := pb.NewFileShareClient(conn)
 	orcaClient.RequestFileFromMarket(client, &pb.FileDesc{})
+
+	serverIP = "localhost:50052"
+	go SetupTestMarket()
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err = grpc.Dial(serverIP, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	orcaClient.RecordTransactionWrapper(client, &pb.FileRequestTransaction{FileByteSize: 100,
+		FileHashName:      "abc",
+		CurrencyExchanged: float32(1),
+		SenderId:          "s1",
+		ReceiverId:        "r1",
+		FileIpLocation:    "localhost:50051",
+		SecondsTimeout:    100,
+	})
 }
