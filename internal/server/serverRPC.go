db9861f0abd764b6d6c1e80d7a236c91b92d2b09
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-03-22 13:25:11 -0400

    Add: DHT Creation and functions to store and put [TAKEN FROM STARFISH]

--- internal/server/serverRPC.go
+++ internal/server/serverRPC.go
@@ -7,17 +7,175 @@ import (
 	"log"
 	"net/http"
 	"orca-peer/internal/fileshare"
-	pb "orca-peer/internal/fileshare"
 	"os"
 	"sync"
 	"time"
+
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	record "github.com/libp2p/go-libp2p-record"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
+	"github.com/multiformats/go-multiaddr"
 )
 
+type OrcaValidator struct{}
+
+// testing, will actually validate later
+func (v OrcaValidator) Validate(key string, value []byte) error {
+	return nil
+}
+
+func (v OrcaValidator) Select(key string, value [][]byte) (int, error) {
+	return 0, nil
+}
+
 type fileShareServerNode struct {
-	pb.UnimplementedFileShareServer
-	savedFiles   map[string][]*pb.FileDesc // read-only after initialized
-	mu           sync.Mutex                // protects routeNotes
+	fileshare.UnimplementedFileShareServer
+	savedFiles   map[string][]*fileshare.FileDesc // read-only after initialized
+	mu           sync.Mutex                       // protects routeNotes
 	currentCoins float64
+}
+
+func CreateDHTConnection(bootstrapAddress *string) (context.Context, *dht.IpfsDHT) {
+	bootstrapPeer := "/ip4/209.151.153.224/tcp/44981/p2p/QmanpBhRCMNk4hFsuYPFdA1JF4VG9yR4w19N17foG3C7CB"
+	if *bootstrapAddress != "" {
+		bootstrapPeer = *bootstrapAddress
+	}
+	isClient := false
+
+	ctx := context.Background()
+
+	//Generate private key for peer
+	privKey, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	//Construct multiaddr from string and create host to listen on it
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/44981")
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings(sourceMultiAddr.String()),
+		libp2p.Identity(privKey), //derive id from private key
+	}
+	host, err := libp2p.New(opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Host ID: %s", host.ID())
+	log.Printf("Connect to me on:")
+	for _, addr := range host.Addrs() {
+		log.Printf("%s/p2p/%s", addr, host.ID())
+	}
+
+	//An array if we want to expand to a more stable peer list instead of providing in args
+	bootstrapPeers := []string{
+		bootstrapPeer,
+	}
+
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	var validator record.Validator = OrcaValidator{}
+	var options []dht.Option
+	if isClient { //if no bootstrap peer, go into server mode
+		options = append(options, dht.Mode(dht.ModeClient))
+	} else {
+		options = append(options, dht.Mode(dht.ModeServer))
+	}
+	options = append(options, dht.ProtocolPrefix("orcanet/market"), dht.Validator(validator))
+	kDHT, err := dht.New(ctx, host, options...)
+	if err != nil {
+		panic(err)
+	}
+
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	log.Println("Bootstrapping the DHT")
+	if err = kDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddrString := range bootstrapPeers {
+		if peerAddrString == "" {
+			continue
+		}
+		peerAddr, err := multiaddr.NewMultiaddr(peerAddrString)
+		if err != nil {
+			panic(err)
+		}
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				log.Println("WARNING: ", err)
+			} else {
+				log.Println("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+	go discoverPeers(ctx, host, kDHT, "orcanet/market")
+	time.Sleep(5 * time.Second)
+
+	return ctx, kDHT
+}
+func PlaceKey(ctx context.Context, kDHT *dht.IpfsDHT, putKey string, putValue string) {
+	err := kDHT.PutValue(ctx, "orcanet/market/"+putKey, []byte(putValue))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		time.Sleep(5 * time.Second)
+		return
+	}
+	fmt.Println("Put key: ", putKey+" Value: "+putValue)
+}
+func SearchKey(ctx context.Context, kDHT *dht.IpfsDHT, searchKey string) {
+	valueStream, err := kDHT.SearchValue(ctx, "orcanet/market/"+searchKey)
+	fmt.Println("Searching for " + searchKey)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		time.Sleep(5 * time.Second)
+		return
+	}
+	time.Sleep(5 * time.Second)
+	for byteArray := range valueStream {
+		fmt.Println(string(byteArray))
+	}
+}
+func discoverPeers(ctx context.Context, h host.Host, kDHT *dht.IpfsDHT, advertise string) {
+	routingDiscovery := drouting.NewRoutingDiscovery(kDHT)
+	dutil.Advertise(ctx, routingDiscovery, advertise)
+
+	// Look for others who have announced and attempt to connect to them
+	for {
+		fmt.Println("Searching for peers...")
+		peerChan, err := routingDiscovery.FindPeers(ctx, advertise)
+		if err != nil {
+			panic(err)
+		}
+		for peer := range peerChan {
+			if peer.ID == h.ID() {
+				continue // No self connection
+			}
+			err := h.Connect(ctx, peer)
+			if err != nil {
+				fmt.Printf("Failed connecting to %s, error: %s\n", peer.ID, err)
+			} else {
+				fmt.Println("Connected to:", peer.ID)
+			}
+		}
+		time.Sleep(time.Second * 10)
+	}
 }
 
 func sendFileToConsumer(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +224,7 @@ func sendFileToConsumer(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func runNotifyStore(client pb.FileShareClient, file *pb.FileDesc) *fileshare.StorageACKResponse {
+func runNotifyStore(client fileshare.FileShareClient, file *fileshare.FileDesc) *fileshare.StorageACKResponse {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	ackResponse, err := client.NotifyFileStore(ctx, file)
@@ -77,7 +235,7 @@ func runNotifyStore(client pb.FileShareClient, file *pb.FileDesc) *fileshare.Sto
 	return ackResponse
 }
 
-func runNotifyUnstore(client pb.FileShareClient, file *pb.FileDesc) *fileshare.StorageACKResponse {
+func runNotifyUnstore(client fileshare.FileShareClient, file *fileshare.FileDesc) *fileshare.StorageACKResponse {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	ackResponse, err := client.NotifyFileUnstore(ctx, file)
@@ -88,8 +246,8 @@ func runNotifyUnstore(client pb.FileShareClient, file *pb.FileDesc) *fileshare.S
 	return ackResponse
 }
 
-func NotifyStoreWrapper(client pb.FileShareClient, file_name_hash string, file_name string, file_size_bytes int64, file_origin_address string, origin_user_id string, file_cost float32, file_data_hash string, file_bytes []byte) {
-	var file_description = pb.FileDesc{FileNameHash: file_name_hash,
+func NotifyStoreWrapper(client fileshare.FileShareClient, file_name_hash string, file_name string, file_size_bytes int64, file_origin_address string, origin_user_id string, file_cost float32, file_data_hash string, file_bytes []byte) {
+	var file_description = fileshare.FileDesc{FileNameHash: file_name_hash,
 		FileName:          file_name,
 		FileSizeBytes:     file_size_bytes,
 		FileOriginAddress: file_origin_address,
@@ -104,8 +262,8 @@ func NotifyStoreWrapper(client pb.FileShareClient, file_name_hash string, file_n
 		fmt.Printf("[Server]: Unable to notify market that we are stopping the storage of file %s with hash %s \n", ack.FileName, ack.FileHash)
 	}
 }
-func NotifyUnstoreWrapper(client pb.FileShareClient, file_name_hash string, file_name string, file_size_bytes int64, file_origin_address string, origin_user_id string, file_cost float32, file_data_hash string, file_bytes []byte) {
-	var file_description = pb.FileDesc{FileNameHash: file_name_hash,
+func NotifyUnstoreWrapper(client fileshare.FileShareClient, file_name_hash string, file_name string, file_size_bytes int64, file_origin_address string, origin_user_id string, file_cost float32, file_data_hash string, file_bytes []byte) {
+	var file_description = fileshare.FileDesc{FileNameHash: file_name_hash,
 		FileName:          file_name,
 		FileSizeBytes:     file_size_bytes,
 		FileOriginAddress: file_origin_address,
