6601a36107cdac64c0efde582531b14b802a1d27
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-01 11:56:22 -0400

    Remove: public/private key code in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	orcaCLI "orca-peer/internal/cli"
 	orcaHash "orca-peer/internal/hash"
 	orcaTest "orca-peer/test"
@@ -18,26 +16,6 @@ func main() {
 	flag.StringVar(&boostrapNodeAddress, "bootstrap", "", "Give address to boostrap.")
 	flag.Parse()
 	publicKey, privateKey := orcaHash.LoadInKeys()
-	cost := orcaHash.GeneratePriceBytes(100)
-	signed, err := orcaHash.SignFile(cost, privateKey)
-	if err != nil {
-		fmt.Println("Error signing file")
-	}
-	jsonData := map[string]interface{}{
-		"key1": 10,
-	}
-	// Marshal JSON object to byte array
-	jsonBytes, err := json.Marshal(jsonData)
-	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return
-	}
-	suc := orcaHash.VerifySignature(signed, jsonBytes, publicKey)
-	if suc == nil {
-		fmt.Println("Valid Transaction")
-	} else {
-		fmt.Println("Invalid Transaction")
-	}
 	os.MkdirAll("./files/stored/", 0755)
 	if test {
 		orcaTest.RunTestServer()
